3fc886fa263fe64dcee8052d9782c2f9e32a72f5
kent riggs <k2r3@proton.me>
2024-01-10 17:52:47 -0700

    added code to satisfy tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+saturdayFun();
+function saturdayFun(activity = "roller-skate") {
+    return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(mondayLocation = "go to the office") {
+    return(`This Monday, I will ${mondayLocation}.`)
+}
+mondayWork("will work from home")
+
+function wrapAdjective(parameter = "*") {
+return function (adjective = "special") {
+return (`You are ${parameter}${adjective}${parameter}!`)    
+    }
+}
+const encouragingPromptFunction = wrapAdjective("||")
+console.log(encouragingPromptFunction())
